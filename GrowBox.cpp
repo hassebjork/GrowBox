5088c5683baed03bb0afb3bbee3be7386fe71ad9
Hasse Björk <hassebjork@users.noreply.github.com>
2018-10-07 00:43:04 +0200

    All variables moved into GrowBox.h

--- GrowBox.cpp
+++ GrowBox.cpp
@@ -1,22 +1,117 @@
-#include <Arduino.h>
 #include "GrowBox.h"
-#include <Wire.h>
 
 const char * fetName[] = { "Fan1", "Fan2", "Light", "Pump" };
 const char   fetPin[]  = { 2, 0, 15, 13 };
 
 GrowBox::GrowBox() {
+  init();
+}
+
+void GrowBox::init() {
+#ifdef COM
+  Serial.begin( 115200 );
+  Serial.println();
+  Serial.println("Booting Sketch...");
+#else
+  pinMode( TX,  OUTPUT );
+  pinMode( RX,  OUTPUT );
+#endif
+
+#ifdef I2C
+  Wire.begin( SDA, SCL );
+  oled.begin( &Adafruit128x64, I2C_OLED );
+  oled.set400kHz();  
+  oled.setFont(Adafruit5x7);  
+#else
+  pinMode( SDA, OUTPUT );
+  pinMode( SCL, OUTPUT );
+#endif
+  
+  pinMode( IO12, INPUT );
+  pinMode( IO14, INPUT );
+  
   config.fetState       = 0;
   config.humidity       = 0;
-  config.temperature.x  = 20.0;
-  config.temperature.r  = 0.2;
-  config.temperature.pn = 0.1;
-  config.temperature.p  = 1.0;
-
+  config.temperature.x  = 20.0; // Predicted value
+  config.temperature.r  = 0.2;  // Sensor noise
+  config.temperature.pn = 0.2;  // Process noise
+  config.temperature.p  = 1.0;  // Predicted error
+  config.previousMillis = 0;
+  
   // Initiate and switch all FETs off
   for ( char fetNo = 0; fetNo < sizeof( fetPin ) - 1; fetNo++ ) {
     pinMode( fetPin[fetNo], OUTPUT );
     digitalWrite( fetPin[fetNo], LOW );
+  }
+  
+}
+
+void GrowBox::update() {
+  unsigned long currentMillis = millis();
+  char    curTime[13];
+  char    buff[60];
+
+  snprintf ( curTime, 60, "%02d %02d:%02d:%02d",
+    (int)( currentMillis / 3600000 ) / 24, 
+    (int)( currentMillis / 3600000 ) % 24,
+    (int)( currentMillis / 60000 ) % 60,
+    (int)( currentMillis / 1000 ) % 60 );
+
+  
+  if ( currentMillis - config.previousMillis >= INTERVAL ) {
+    config.previousMillis += INTERVAL;
+#ifdef I2C
+    uint8_t t = dht12get();
+    
+    oled.home();
+    oled.print("Time:");
+    oled.setCursor( 40, 0 );
+    oled.print( curTime );
+    oled.clearToEOL();
+    
+    if ( t == 0 ) {
+      oled.setCursor( 0, 1 );
+      oled.print( "Temp: " );
+      oled.setCursor( 40, 1 );
+      oled.print( temp, 1 );
+      oled.clearToEOL();
+      oled.setCursor( 0, 2 );
+      oled.print( "Humid: " );
+      oled.setCursor( 40, 2 );
+      oled.print( config.humidity );
+      oled.clearToEOL();
+      oled.setCursor( 0, 3 );
+      oled.print( "x:" );
+      oled.print( config.temperature.x, 5 );
+      oled.setCursor( 64, 3 );
+      oled.print( "r:" );
+      oled.print( config.temperature.r, 5 );
+      oled.setCursor( 0, 4 );
+      oled.print( "n:" );
+      oled.print( config.temperature.pn, 5 );
+      oled.setCursor( 64, 4 );
+      oled.print( "p:" );
+      oled.print( config.temperature.p, 5 );
+    } else {
+      oled.setCursor( 0, 1 );
+      oled.print( "DHT12 error: " );
+      oled.println( temp );
+      oled.clearToEOL();
+    }
+#endif
+  
+#ifdef COM
+    snprintf ( buff, 60, "\"%s\",%.1f,%.3f,%.3f,%.3f,%f",
+      curTime,
+      temp,
+      config.temperature.x,
+      config.temperature.r,
+      config.temperature.pn,
+      config.temperature.p
+    );
+    Serial.println( buff );
+#endif
+    
   }
 }
 
@@ -41,15 +136,15 @@ char GrowBox::fetStatus( char fetNo ) {
   return BIT_CHECK( config.fetState, fetNo );
 }
 
-uint8_t GrowBox::dht12get( uint8_t address ) {
+uint8_t GrowBox::dht12get() {
   uint8_t data[5];
   
-  Wire.beginTransmission( address );
+  Wire.beginTransmission( (uint8_t) I2C_DHT12 );
   Wire.write(0);
   if ( Wire.endTransmission() != 0 )
     return 1; // Error connect
   
-  Wire.requestFrom( address, (uint8_t) 5 );
+  Wire.requestFrom( (uint8_t) I2C_DHT12, (uint8_t) 5 );
   for ( uint8_t i = 0; i < 5; i++ )
     data[i] = Wire.read();
   delay( 50 );
--- GrowBox.h
+++ GrowBox.h
@@ -1,6 +1,7 @@
 #ifndef _GrowBox_h
 #define _GrowBox_h
 
+#include <Arduino.h>
 #include "Kalman.h"
 
 #define BIT_SET(a,b) ((a) |= (1<<(b)))
@@ -8,10 +9,31 @@
 #define BIT_FLIP(a,b) ((a) ^= (1<<(b)))
 #define BIT_CHECK(a,b) ((a) & (1<<(b)))
 
-#define FAN1 0
-#define FAN2 1
-#define LED  2
-#define AUX  3
+#define COM
+#define I2C
+
+#define INTERVAL 1000
+/* CONTROL */
+#define SDA  4
+#define SCL  5
+#define TX   1
+#define RX   3
+/* INPUT */
+#define IO12 12
+#define IO14 14
+/* OUTPUT */
+#define FAN1     0
+#define FAN2     1
+#define LED      2
+#define AUX      3
+
+#ifdef I2C
+#define I2C_OLED  0x3C
+#define I2C_DHT12 0x5C
+#include <Wire.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
+#endif
 
 typedef struct {
   uint8_t fetState;     // Last state of FETs
@@ -21,21 +43,24 @@ typedef struct {
   uint8_t maxHumid;
   float   minTemp;
   float   maxTemp;
-  long    interval;    // Update freq
+  unsigned long previousMillis;
 } Config;
 
 class GrowBox {
 public:
   Config config;
   float  temp;
+  SSD1306AsciiWire oled;
   
   GrowBox();
+  void init();
+  void update();
   void fetSet( char fetNo, char stat );
   void fetOn( char fetNo );
   void fetOff( char fetNo );
   char fetStatus( char fetNo );
   
-  uint8_t dht12get( uint8_t address );
+  uint8_t dht12get();
 };
 
 #endif
