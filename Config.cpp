05239b2e83a4216ef53a0318af76453603b6a4df
Hasse Bjork <hassebjork@users.noreply.github.com>
2019-03-30 22:43:19 +0100

    Removed ArduinoJson Added json-streaming-parser (https://github.com/squix78/json-streaming-parser) Fixed default values
    Signed-off-by: Hasse Bjork <hassebjork@users.noreply.github.com>

--- Config.cpp
+++ Config.cpp
@@ -27,125 +27,132 @@ const char attr_logTime[]    PROGMEM = "logTime";
 const char attr_updateTime[] PROGMEM = "updateTime";
 const char attr_dimStep[]    PROGMEM = "dimStep";
 const char* Config::attr[]   PROGMEM = {
-  attr_name, attr_tempMax, attr_humidMax, attr_tz,
-  attr_dst, attr_ledOn, attr_ledOff, attr_logTime,
-  attr_updateTime, attr_dimStep
+	attr_name, attr_tempMax, attr_humidMax, attr_tz,
+	attr_dst, attr_ledOn, attr_ledOff, attr_logTime,
+	attr_updateTime, attr_dimStep
 };
 
 const char Config::config_file[] PROGMEM = "/growbox.json";
+const char Config::config_old[]  PROGMEM = "/growbox.old";
 
 Config::Config() {
-  ledOn.hour    = 26;
-  ledOn.minute  = 60;
-  ledOff.hour   = 26;
-  ledOff.minute = 60;
-  load();
+	ledOn.hour    = 6;
+	ledOn.minute  = 0;
+	ledOff.hour   = 20;
+	ledOff.minute = 0;
+	load();
 }
 Config::~Config() {
 }
-  
+
+void Config::set( const char *key, const char *value ) {
+	DEBUG_MSG("Config::set( '%s', '%s' )\n", key, value );  
+	for ( uint8_t i = NAME; i < attrNo; i++ ) {
+		if ( strcmp( key, attr[i] ) == 0 ) {
+			set( i, value );
+			break;
+		}
+	}
+}
+
 void Config::set( uint8_t d, const char *c ) {
-  DEBUG_MSG("Config::set( %d, '%s' )\n", d, c );  
-  
-  switch( d ) {
-    
-    case NAME: {
-      saved = ( strcmp( name, c ) == 0 );
-      strlcpy( name, c, sizeof( name ) );
-    }
-    break;
-    
-    case TEMPMAX: {
-      float f = atof( c );
-      if ( f > 0.0 && f < 50.0 ) {
-        saved = ( f == tempMax );
-        tempMax = f;
-      }
-    }
-    break;
-    
-    case HUMIDMAX: {
-      uint8_t i = (uint8_t) atoi( c );
-      if ( i >= 0 && i <= 100 ) {
-        saved = ( i == humidMax );
-        humidMax = i;
-      }
-    }
-    break;
-    
-    case TZ: {
-      int8_t i = atoi( c );
-      if ( i > -13 && i < 13 ) {
-        saved = ( i == tz );
-        tz = i;
-      }
-    }
-    break;
-    
-    case DST:
-      saved = setBool( dst, c );
-    break;
-    
-    case LEDON:
-      saved = setAlarm( ledOn, c );
-    break;
-    
-    case LEDOFF:
-      saved = setAlarm( ledOff, c );
-    break;
-    
-    case LOGTIME: {     // Set in seconds
-      unsigned long i = atoi( c ) * 1000;
-      if ( i >= 0 ) {
-        saved = ( i == logTime );
-        logTime = i;
-      }
-    }
-    break;
-    
-    case UPDATETIME: {  // Set in seconds
-      unsigned long i = atoi( c ) * 1000;
-      if ( i > 0 ) {
-        saved = ( i == updateTime );
-        updateTime = i;
-      }
-    }
-    break;
-    
-    case DIMSTEP: {
-      uint8_t i = atoi( c );
-      if ( i <= 200 ) {
-        saved = ( i == dimStep );
-        dimStep = i;
-      }
-    }
-    break;
-    
-  }
+	DEBUG_MSG("Config::set( %d, '%s' )\n", d, c );  
+	
+	switch( d ) {
+		
+		case NAME: {
+			saved = ( strcmp( name, c ) == 0 );
+			strlcpy( name, c, sizeof( name ) );
+		}
+		break;
+		
+		case TEMPMAX: {
+			float f = atof( c );
+			if ( f > 0.0 && f < 50.0 ) {
+				saved = ( f == tempMax );
+				tempMax = f;
+			}
+		}
+		break;
+		
+		case HUMIDMAX: {
+			uint8_t i = (uint8_t) atoi( c );
+			if ( i >= 0 && i <= 100 ) {
+				saved = ( i == humidMax );
+				humidMax = i;
+			}
+		}
+		break;
+		
+		case TZ: {
+			int8_t i = atoi( c );
+			if ( i > -13 && i < 13 ) {
+				saved = ( i == tz );
+				tz = i;
+			}
+		}
+		break;
+		
+		case DST:
+			saved = setBool( dst, c );
+		break;
+		
+		case LEDON:
+			saved = setAlarm( ledOn, c );
+		break;
+		
+		case LEDOFF:
+			saved = setAlarm( ledOff, c );
+		break;
+		
+		case LOGTIME: {     // Set in seconds
+			unsigned long i = atol( c ) * 1000;
+			saved = ( i == logTime );
+			logTime = i;
+		}
+		break;
+		
+		case UPDATETIME: {  // Set in seconds
+			unsigned long i = atol( c ) * 1000;
+			saved = ( i == updateTime );
+			updateTime = i;
+		}
+		break;
+		
+		case DIMSTEP: {
+			uint8_t i = atoi( c );
+			if ( i <= 200 ) {
+				saved = ( i == dimStep );
+				dimStep = i;
+			}
+		}
+		break;
+		
+	}
 }
 
 bool Config::setBool( bool &b, const char *c ) {
-  uint8_t i = atoi( c );
-  bool saved;
-  if ( i == 0 || i == 1 ) {
-    saved = ( i == b );
-    b = i;
-  }
-  return saved;
+	uint8_t i = atoi( c );
+	bool saved;
+	if ( i == 0 || i == 1 ) {
+		saved = ( i == b );
+		b = i;
+	}
+	return saved;
 }
 
 bool Config::setAlarm( Alarm &a, const char *c ) {
-  uint8_t hour   = a.hour;
-  uint8_t minute = a.minute;
-  int    i = atoi( c );
-  if ( i >= 0 && i <= 2359 ) {
-    a.hour    = i / 100;
-    a.minute  = i % 100;
-  } else {
-    a.hour    = 25;
-    a.minute  = 00;
-  }
-  return ( hour == a.hour && minute == a.minute );
+	uint8_t hour   = a.hour;
+	uint8_t minute = a.minute;
+	int    i = atoi( c );
+	if ( i >= 0 && i <= 2359 ) {
+		a.hour    = i / 100;
+		a.minute  = i % 100;
+	} else {
+		a.hour    = 25;
+		a.minute  = 00;
+	}
+	return ( hour == a.hour && minute == a.minute );
 }
 
 time_t Config::today( uint16_t hrmin = 0 ) {
@@ -164,18 +171,18 @@ time_t Config::today( uint8_t hr = 0, uint8_t min = 0, uint8_t sec = 0 ) {
 }
 
 bool Config::checkDst() {
-  if ( !dst ) 
-    return false;
-  time_t t = now() + tz * SECS_PER_HOUR;
-  if ( month(t)  < 3 || month(t) > 10 )       // Jan, Feb, Nov, Dec 
-    return false;
-  if ( month(t)  > 3 && month(t) < 10 )       // Apr, Jun; Jul, Aug, Sep 
-    return true;
-  if ( month(t) ==  3 && ( hour(t) + 24 * day(t) ) >= ( 3 +  24 * ( 31 - ( 5 * year(t) / 4 + 4 ) % 7 ) ) 
-    || month(t) == 10 && ( hour(t) + 24 * day(t) ) <  ( 3 +  24 * ( 31 - ( 5 * year(t) / 4 + 1 ) % 7 ) ) )
-    return true;
-  else
-    return false;
+	if ( !dst ) 
+		return false;
+	time_t t = now() + tz * SECS_PER_HOUR;
+	if ( month(t)  < 3 || month(t) > 10 )       // Jan, Feb, Nov, Dec 
+		return false;
+	if ( month(t)  > 3 && month(t) < 10 )       // Apr, Jun; Jul, Aug, Sep 
+		return true;
+	if ( month(t) ==  3 && ( hour(t) + 24 * day(t) ) >= ( 3 +  24 * ( 31 - ( 5 * year(t) / 4 + 4 ) % 7 ) ) 
+		|| month(t) == 10 && ( hour(t) + 24 * day(t) ) <  ( 3 +  24 * ( 31 - ( 5 * year(t) / 4 + 1 ) % 7 ) ) )
+		return true;
+	else
+		return false;
 }
 
 void Config::timeRefresh() {
@@ -185,19 +192,18 @@ void Config::timeRefresh() {
 }
 
 void Config::toJson( char *c, int size ) {
-  strncat( c, "{", size );
-  toJson( c, NAME, name, size );
-  toJson( c, HUMIDMAX, humidMax, size );
-  toJson( c, TEMPMAX, tempMax, size );
-  toJson( c, TZ, tz, size );
-  toJson( c, DST, dst, size );
-  toJson( c, LEDON, (uint16_t)(ledOn.hour * 100) + ledOn.minute, size );
-  toJson( c, LEDOFF, (uint16_t)(ledOff.hour * 100) + ledOff.minute, size );
-  toJson( c, LOGTIME, (int)( logTime / 1000 ), size );
-  toJson( c, UPDATETIME, (int)( updateTime / 1000 ), size );
-  toJson( c, DIMSTEP, dimStep, size );
-  strncat( c, "}", size );
-//   DEBUG_MSG("Config::toJson: %d bytes %d buff\n", strlen( c ), size );
+	strncat( c, "{", size );
+	toJson( c, NAME, name, size );
+	toJson( c, HUMIDMAX, humidMax, size );
+	toJson( c, TEMPMAX, tempMax, size );
+	toJson( c, TZ, tz, size );
+	toJson( c, DST, dst, size );
+	toJson( c, LEDON, (uint16_t)(ledOn.hour * 100) + ledOn.minute, size );
+	toJson( c, LEDOFF, (uint16_t)(ledOff.hour * 100) + ledOff.minute, size );
+	toJson( c, LOGTIME, (int)( logTime / 1000 ), size );
+	toJson( c, UPDATETIME, (int)( updateTime / 1000 ), size );
+	toJson( c, DIMSTEP, dimStep, size );
+	strncat( c, "}", size );
 }
 void Config::jsonAttribute( char *c, ATTR a, int size ) {
 	jsonAttribute( c, attr[a], a != NAME, size );
@@ -210,84 +216,86 @@ void Config::jsonAttribute( char *c, const char *a, bool k, int size ) {
 }
 // String
 void Config::toJson( char *c, const char* s, int size ) {
-  strncat( c, "\"", size );
-  strncat( c, s, size );
-  strncat( c, "\"", size );
+	strncat( c, "\"", size );
+	strncat( c, s, size );
+	strncat( c, "\"", size );
 }
 void Config::toJson( char *c, ATTR a, const char* s, int size ) {
-  jsonAttribute( c, a, size );
-  toJson( c, s, size );
+	jsonAttribute( c, a, size );
+	toJson( c, s, size );
 }
 // Integer
 void Config::toJson( char *c, int i, int size ) {
-  char buff[10];
-  itoa( i, buff, 10 );
-  strncat( c, buff, size );
+	char buff[10];
+	itoa( i, buff, 10 );
+	strncat( c, buff, size );
 }
 void Config::toJson( char *c, ATTR a, int i, int size ) {
-  jsonAttribute( c, a, size );
-  toJson( c, i, size );
+	jsonAttribute( c, a, size );
+	toJson( c, i, size );
 }
 // Float
 void Config::toJson( char *c, float f, int size ) {
-  char buff[10];
-  dtostrf( f, 0, 1, buff );
-  strncat( c, buff, size );
+	char buff[10];
+	dtostrf( f, 0, 1, buff );
+	strncat( c, buff, size );
 }
 void Config::toJson( char *c, ATTR a, float f, int size ) {
-  jsonAttribute( c, a, size );
-  toJson( c, f, size );
+	jsonAttribute( c, a, size );
+	toJson( c, f, size );
 }
 
 void Config::load() {
-  DEBUG_MSG("Config::load()\n" );  
-  SPIFFS.begin();
-  File f = SPIFFS.open( FPSTR( config_file ), "r" );
-  if ( !f ) {
-    DEBUG_MSG("Config::load error: No file!\n" );  
-    return;
-  }
-  char c[6];
-  StaticJsonDocument<512> doc;
-  DeserializationError error = deserializeJson( doc, f );
-  if (error) {
-    DEBUG_MSG("Config::load error: Deserialization failed!\n" );  
-  } else {
-    JsonObject root = doc.as<JsonObject>();
-    set( NAME,       root[attr[NAME]]       | "DefName" );
-    set( HUMIDMAX,   root[attr[HUMIDMAX]]   | "85" );
-    set( TEMPMAX,    root[attr[TEMPMAX]]    | "28.0" );
-    set( TZ,         root[attr[TZ]]         | "1"  );
-    set( DST,        root[attr[DST]]        | "1"  );
-    set( LOGTIME,    root[attr[LOGTIME]]    | "3600"  );
-    set( UPDATETIME, root[attr[UPDATETIME]] | "1"  );
-    set( DIMSTEP,    root[attr[DIMSTEP]]    | "20" );
-    itoa( root[attr[LEDON]  ] | 600, c, 10 );
-    set( LEDON,    c );
-    itoa( root[attr[LEDOFF] ] | 2000, c, 10 );
-    set( LEDOFF,   c );
-  }
-  f.close();
+	DEBUG_MSG("Config::load()\n" );  
+	SPIFFS.begin();
+	File f = SPIFFS.open( FPSTR( config_file ), "r" );
+	if ( !f ) {
+		DEBUG_MSG("Config::load error: No file!\n" );  
+		return;
+	}
+	
+	JsonStreamingParser parser;
+	parser.setListener( this );
+	
+	while( f.available() )
+		parser.parse( f.read() );
+	
+	f.close();
 }
   
 void Config::save() {
-  if ( saved )
-    return;
-  SPIFFS.begin();
-  if ( SPIFFS.exists( FPSTR( config_file ) ) ) {
-    if ( !SPIFFS.rename( FPSTR( config_file ), F( "/growbox.old" ) ) )
-      DEBUG_MSG( "Config::save error: Faild to rename file!\n" );  
-  }
-  File file = SPIFFS.open( FPSTR( config_file ), "w" );
-  if ( !file ) {
-    DEBUG_MSG( "Config::save error: Faild to open file!\n" );  
-    return;
-  }
-  char temp[200];
-  toJson( temp, 200 );
-  file.print( temp );
-  file.close();
-  saved = true;
-  DEBUG_MSG( "Config::save()\n" );  
+	if ( saved )
+		return;
+	SPIFFS.begin();
+	if ( SPIFFS.exists( FPSTR( config_file ) ) ) {
+		if ( SPIFFS.exists( FPSTR( config_old ) ) )
+			SPIFFS.remove( FPSTR( config_old ) );
+		if ( !SPIFFS.rename( FPSTR( config_file ), FPSTR( config_old ) ) )
+			DEBUG_MSG( "Config::save error: Faild to rename file!\n" );  
+	}
+	File file = SPIFFS.open( FPSTR( config_file ), "w" );
+	if ( !file ) {
+		DEBUG_MSG( "Config::save error: Faild to open file!\n" );  
+		return;
+	}
+	char temp[200];
+	toJson( temp, 200 );
+	file.print( temp );
+	file.close();
+	saved = true;
+	DEBUG_MSG( "Config::save()\n" );  
 }
 
+void Config::key( String key ) {
+	_key = key;
+}
+void Config::value( String value ) {
+	set( _key.c_str(), value.c_str() );
+}
+void Config::whitespace( char c ) {}
+void Config::startDocument() {}
+void Config::endArray() {}
+void Config::endObject() {}
+void Config::endDocument() {}
+void Config::startArray() {}
+void Config::startObject() {}
--- Config.h
+++ Config.h
@@ -1,11 +1,4 @@
 /*
- * ArduinoJson
- * https://arduinojson.org/v5/example/config/
- * 
- * Binary Files
- * https://stackoverflow.com/questions/5506645/how-to-read-write-a-struct-in-binary-files
- * https://stackoverflow.com/questions/11928053/read-write-a-complex-struct-into-the-binary-file
- * https://stackoverflow.com/a/46948683/4405465
  * 
  */
 
@@ -18,12 +11,13 @@
 #ifndef _Config_h
 #define _Config_h
 
-#include "FS.h"           // https://github.com/esp8266/Arduino/tree/master/cores/esp8266
-#include "ArduinoJson.h"  // https://arduinojson.org/ https://arduinojson.org/assistant/
-#include <pgmspace.h>     // PROGMEM functions
-#include <TimeLib.h>      // https://github.com/PaulStoffregen/Time
-#include <string.h>       // strncat etc
-
+#include "FS.h"                    // https://github.com/esp8266/Arduino/tree/master/cores/esp8266
+#include "JsonStreamingParser.h"   // https://github.com/squix78/json-streaming-parser
+#include "JsonListener.h"
+#include <pgmspace.h>              // PROGMEM functions
+#include <TimeLib.h>               // https://github.com/PaulStoffregen/Time
+#include <string.h>                // strncat etc
+#include <stdlib.h>                // atoi, atol
 typedef struct {
   uint8_t hour;
   uint8_t minute;
@@ -33,53 +27,66 @@ typedef struct {
   // https://stackoverflow.com/questions/18145874/passing-a-pointer-to-a-class-member-function-as-a-parameter
 } Alarm;
 
-class Config {
-public:
-  static const char *attr[];
-  static const char *months[];
-  static const char config_file[];
-  enum ATTR {
-    NAME, TEMPMAX, HUMIDMAX, TZ,
-	DST, LEDON, LEDOFF, LOGTIME,
-	UPDATETIME, DIMSTEP, attrNo		// Only json-file change
-  };
-                                // Controller name
-  char       name[10]      = {'D','e','f','N','a','m','e','\0' }; // Bugfix for gcc 4.9
-  uint8_t    humidMax      = 85;     // Humidity High
-  float      tempMax       = 28.0;   // Temperature High
-  int8_t     tz            = 1;      // TimeZone hours
-  bool       dst           = true;   // Daylight Saving Time
-  bool       saved         = true;   // Configuration data saved true/false
-  time_t     time;                   // Current time
-  unsigned long logTime    = 3600;   // Milliseconds between log records 0=off
-  unsigned long updateTime = 1000;   // Milliseconds between Growbox.update
-  unsigned long updMillis  = 0;      // Time to next run of Growbox.update in ms
-  uint8_t    dimStep       = 10;     // Dimmer steps
-  
-  Alarm      ledOn;                 //
-  Alarm      ledOff;                //
+class Config: public JsonListener {
+	public:
+	static const char *attr[];
+	static const char *months[];
+	static const char config_file[];
+	static const char config_old[];
+	enum ATTR {
+		NAME, TEMPMAX, HUMIDMAX, TZ,
+		DST, LEDON, LEDOFF, LOGTIME,
+		UPDATETIME, DIMSTEP, attrNo	    // Change by json-file only
+	};
+                                        // Controller name
+	char       name[10]      = {'G','r','o','w','B','o','x','\0' }; // Bugfix for gcc 4.9
+	uint8_t    humidMax      = 85;      // Humidity High
+	float      tempMax       = 28.0;    // Temperature High
+	int8_t     tz            = 1;       // TimeZone hours
+	bool       dst           = true;    // Daylight Saving Time
+	bool       saved         = true;    // Configuration data saved true/false
+	time_t     time;                    // Current time
+	unsigned long logTime    = 3600000; // Milliseconds between log records 0=off
+	unsigned long updateTime = 1000;    // Milliseconds between Growbox.update
+	unsigned long updMillis  = 0;       // Time to next run of Growbox.update in ms
+	uint8_t    dimStep       = 10;      // Dimmer steps
+	String     _key          = "";	    // JSON key:value name
+	
+	Alarm      ledOn;                   // Alarm time led on
+	Alarm      ledOff;                  // Alarm time led off
 
-  Config();
-  ~Config();
-  void set( uint8_t d, const char *c );
-  bool setAlarm( Alarm &a, const char *c );
-  bool setBool( bool &b, const char *c );
-  
-  time_t today( uint8_t hr, uint8_t min, uint8_t sec );
-  time_t today( uint16_t hrmin );
-  bool checkDst();
-  void timeRefresh();
-  
-  void toJson( char *c, int size );
-  static void jsonAttribute( char *c, const char *a, bool k, int size );
-  void jsonAttribute( char *c, ATTR a, int size );
-  static void toJson( char *c, const char* s, int size );
-  void toJson( char *c, ATTR a, const char* s, int size );
-  static void toJson( char *c, int i, int size );
-  void toJson( char *c, ATTR a, int i, int size );
-  static void toJson( char *c, float f, int size );
-  void toJson( char *c, ATTR a, float f, int size );
-  void load();
-  void save();
+	Config();
+	~Config();
+	void set( const char *key, const char *value );
+	void set( uint8_t d, const char *c );
+	bool setAlarm( Alarm &a, const char *c );
+	bool setBool( bool &b, const char *c );
+	
+	time_t today( uint8_t hr, uint8_t min, uint8_t sec );
+	time_t today( uint16_t hrmin );
+	bool checkDst();
+	void timeRefresh();
+	
+	void toJson( char *c, int size );
+	static void jsonAttribute( char *c, const char *a, bool k, int size );
+	void jsonAttribute( char *c, ATTR a, int size );
+	static void toJson( char *c, const char* s, int size );
+	void toJson( char *c, ATTR a, const char* s, int size );
+	static void toJson( char *c, int i, int size );
+	void toJson( char *c, ATTR a, int i, int size );
+	static void toJson( char *c, float f, int size );
+	void toJson( char *c, ATTR a, float f, int size );
+	void load();
+	void save();
+	
+	virtual void key( String key );
+	virtual void value( String value );
+	virtual void whitespace( char c );
+	virtual void startDocument();
+	virtual void endArray();
+	virtual void endObject();
+	virtual void endDocument();
+	virtual void startArray();
+	virtual void startObject();
 };
 #endif
